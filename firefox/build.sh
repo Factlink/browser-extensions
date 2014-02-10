8ceb424d950c7632e449b77a2eaa7991ef3f967b
Eamon Nerbonne <eamon@nerbonne.org>
2014-02-10 14:33:01 +0100

    Fix typo

--- firefox/build.sh
+++ firefox/build.sh
@@ -3,4 +3,4 @@
 #https://static.factlink.com/extension/firefox/updates.rdf
 #https://static.factlink.com/extension/firefox/factlink-#{self[:version]}.xpi"
 
- cfx xpi https://static.factlink.com/extension/firefox/factlink-extension.xpi --update-url https://static.factlink.com/extension/firefox/updates.rdf
+cfx xpi  --update-link https://static.factlink.com/extension/firefox/factlink-extension.xpi --update-url https://static.factlink.com/extension/firefox/updates.rdf
