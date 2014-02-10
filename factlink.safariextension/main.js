5702d08dcd08e657b8034d53b77143ef7dbaa827
Eamon Nerbonne <eamon@nerbonne.org>
2014-02-10 14:48:15 +0100

    Reduce nesting, speed up loading

--- factlink.safariextension/main.js
+++ factlink.safariextension/main.js
@@ -1,15 +1,14 @@
 function initialize(i) {
   return function() {
-    if(document.head) {
-      if (window.top === window) {
-        var script = document.createElement("script");
-        script.src = "https://static.factlink.com/lib/dist/factlink_loader_publishers.min.js";
-        //use "http://localhost:8000/lib/dist/factlink_loader_publishers.js" for local testing
-        document.head.appendChild(script);
-      }
+    if(!document.head)
+      setTimeout(initialize(i+1), i);
+    else if (window.top === window) {
+      var script = document.createElement("script");
+      script.src = "https://static.factlink.com/lib/dist/factlink_loader_publishers.min.js";
+      //use "http://localhost:8000/lib/dist/factlink_loader_publishers.js" for local testing
+      document.head.appendChild(script);
     }
-    else setTimeout(initialize(i+1), i);
   };
 }
 
-initialize(3)();
+initialize(0)();
