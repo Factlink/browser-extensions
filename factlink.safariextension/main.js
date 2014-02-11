37a3950dfa6c7935133717854d06b522c61b74de
Eamon Nerbonne <eamon@nerbonne.org>
2014-02-11 10:11:19 +0100

    Use new loader URI

--- factlink.safariextension/main.js
+++ factlink.safariextension/main.js
@@ -4,8 +4,8 @@ function initialize(i) {
       setTimeout(initialize(i+1), i);
     else if (window.top === window) {
       var script = document.createElement("script");
-      script.src = "https://static.factlink.com/lib/dist/factlink_loader_publishers.min.js?extension";
-      //use "http://localhost:8000/lib/dist/factlink_loader_publishers.js" for local testing
+      script.src = "https://static.factlink.com/lib/dist/factlink_loader.min.js?o=extension";
+      //use "http://localhost:8000/lib/dist/factlink_loader.js" for local testing
       document.head.appendChild(script);
     }
   };
