7c168bd2cee6b411f8db972b7be17b7d25e49dfc
Eamon Nerbonne <eamon@nerbonne.org>
2014-02-11 09:42:55 +0100

    Allow using the extension on mobile.

--- firefox/build.sh
+++ firefox/build.sh
@@ -3,4 +3,4 @@
 #https://static.factlink.com/extension/firefox/updates.rdf
 #https://static.factlink.com/extension/firefox/factlink-#{self[:version]}.xpi"
 
-cfx xpi  --update-link https://static.factlink.com/extension/firefox/factlink-latest.xpi --update-url https://static.factlink.com/extension/firefox/updates.rdf
+cfx xpi --force-mobile --update-link https://static.factlink.com/extension/firefox/factlink-latest.xpi --update-url https://static.factlink.com/extension/firefox/updates.rdf
