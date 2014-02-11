e8edbf72393c12ebb169648495ffabcd55548d74
Eamon Nerbonne <eamon@nerbonne.org>
2014-02-11 09:59:44 +0100

    Include query string token indicating the publisher script was included by the extension for analysis purposes.

--- factlink.safariextension/main.js
+++ factlink.safariextension/main.js
@@ -4,7 +4,7 @@ function initialize(i) {
       setTimeout(initialize(i+1), i);
     else if (window.top === window) {
       var script = document.createElement("script");
-      script.src = "https://static.factlink.com/lib/dist/factlink_loader_publishers.min.js";
+      script.src = "https://static.factlink.com/lib/dist/factlink_loader_publishers.min.js?extension";
       //use "http://localhost:8000/lib/dist/factlink_loader_publishers.js" for local testing
       document.head.appendChild(script);
     }
--- firefox/data/inject-factlink.js
+++ firefox/data/inject-factlink.js
@@ -1,4 +1,4 @@
 var script = document.createElement("script");
-script.src = "https://static.factlink.com/lib/dist/factlink_loader_publishers.min.js";
+script.src = "https://static.factlink.com/lib/dist/factlink_loader_publishers.min.js?extension";
 //use "http://localhost:8000/lib/dist/factlink_loader_publishers.js" for local testing
 document.head.appendChild(script);
