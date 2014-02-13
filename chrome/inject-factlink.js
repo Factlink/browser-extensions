8f625d5bc0bc10270758a9a8f4bce4cb2c4a258d
Martijn Russchen <martijn@factlink.com>
2014-02-13 09:45:35 +0100

    Wait with injecting javascript until the head is loaded

--- chrome/inject-factlink.js
+++ chrome/inject-factlink.js
@@ -1,4 +1,14 @@
-var script = document.createElement("script");
-script.src = "https://static.factlink.com/lib/dist/factlink_loader.min.js?o=extension";
-//use "http://localhost:8000/lib/dist/factlink_loader.js" for local testing
-document.head.appendChild(script);
+function initialize(i) {
+  return function() {
+    if(!document.head)
+      setTimeout(initialize(i+1), i);
+    else if (window.top === window) {
+      var script = document.createElement("script");
+      script.src = "https://static.factlink.com/lib/dist/factlink_loader.min.js?o=extension";
+      //use "http://localhost:8000/lib/dist/factlink_loader.js" for local testing
+      document.head.appendChild(script);
+    }
+  };
+}
+
+initialize(0)();
