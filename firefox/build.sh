55b0549155c7163b35f28dc1e9945e7cd9a9430c
Martijn Russchen <martijn@factlink.com>
2014-04-28 15:18:37 +0200

    Updated release script

--- firefox/build.sh
+++ firefox/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#https://factlink.github.io/browser-extensions/files/firefox/updates.rdf
-#https://factlink.github.io/browser-extensions/files/firefox/factlink-#{self[:version]}.xpi"
+#https://factlink.github.io/browser-extensions/firefox/updates.rdf
+#https://factlink.github.io/browser-extensions/firefox/factlink-#{self[:version]}.xpi"
 
-cfx xpi --force-mobile --update-link https://factlink.github.io/browser-extensions/files/firefox/factlink-latest.xpi --update-url https://factlink.github.io/browser-extensions/files/firefox/updates.rdf
+cfx xpi --force-mobile --update-link https://factlink.github.io/browser-extensions/firefox/factlink-latest.xpi --update-url https://factlink.github.io/browser-extensions/firefox/updates.rdf
