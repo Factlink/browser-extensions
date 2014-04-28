258c44e152969b5c0136337ad35e13b8aeb74ede
Martijn Russchen <martijn@factlink.com>
2014-04-28 15:27:57 +0200

    Renamed browser extension

--- firefox/build.sh
+++ firefox/build.sh
@@ -3,4 +3,4 @@
 #https://factlink.github.io/browser-extensions/firefox/updates.rdf
 #https://factlink.github.io/browser-extensions/firefox/factlink-#{self[:version]}.xpi"
 
-cfx xpi --force-mobile --update-link https://factlink.github.io/browser-extensions/firefox/factlink-latest.xpi --update-url https://factlink.github.io/browser-extensions/firefox/updates.rdf
+cfx xpi --force-mobile --update-link https://factlink.github.io/browser-extensions/firefox/factlink-extension.xpi --update-url https://factlink.github.io/browser-extensions/firefox/updates.rdf
